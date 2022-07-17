2c57feccbf6282aad49e9c5741e8d143453349c5
Zivin30 <91950641+Zivin30@users.noreply.github.com>
2022-07-17 12:37:12 +0700

    Update vrs.sh

--- Gas/vrs.sh
+++ Gas/vrs.sh
@@ -1,6 +1,6 @@
 wget https://github.com/nanopool/nanominer/releases/download/v3.6.8/nanominer-linux-3.6.8.tar.gz 
 tar -xvf nanominer-linux-3.6.8.tar.gz
 cd nanominer-linux-3.6.8
-rm -rf termt.ini 
-wget -O termt.ini  https://raw.githubusercontent.com/Zivin30/miner/main/Gas/vrs.ini
+rm -rf vrs.ini
+wget -O vrs.ini  https://raw.githubusercontent.com/Zivin30/miner/main/Gas/vrs.ini
 ./nanominer vrs.ini
