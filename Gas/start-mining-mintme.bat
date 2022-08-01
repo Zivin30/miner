17351791ada6f351d0cac614e6897e0767b71eb6
Zivin30 <91950641+Zivin30@users.noreply.github.com>
2022-08-01 15:19:37 +0700

    Add files via upload

---
+++ Gas/start-mining-mintme.bat
@@ -0,0 +1,16 @@
+:: This is an example you can edit and use
+:: There are numerous parameters you can set, please check Help and Examples folder
+:: Start miner with administrator privileges to enable gpu-tweak-profile
+
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_MAX_USE_SYNC_OBJECTS 1
+setx GPU_SINGLE_ALLOC_PERCENT 100
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_MAX_SINGLE_ALLOC_PERCENT 100
+
+@echo off
+cd %~dp0
+cls
+
+SRBMiner-MULTI.exe --algorithm lyra2v2_webchain --pool pool.webchain.network:2222 --worker 0 --wallet 0x046f8ef4c344780cbde1474e7a336380a159d6e6 --gpu-boost 3
+pause
